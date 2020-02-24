656918205b569bb9217a73cf0460116bf6a8a2a3
Sheng Yang <sheng.yang@rancher.com>
2020-02-24 11:08:01 -0800

    Update grpc library generation
    Signed-off-by: Sheng Yang <sheng.yang@rancher.com>

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# check and download dependency for gRPC code generate
 set -e
 
 # check and download dependency for gRPC code generate
-if [ ! -e ./vendor_proto/protobuf/src/google/protobuf ]; then
-    rm -rf ./vendor_proto/protobuf/src/google/protobuf
-    DIR="./vendor_proto/protobuf/src/google/protobuf"
+if [ ! -e ./proto/vendor/protobuf/src/google/protobuf ]; then
+    rm -rf ./proto/vendor/protobuf/src/google/protobuf
+    DIR="./proto/vendor/protobuf/src/google/protobuf"
     mkdir -p $DIR
     wget https://raw.githubusercontent.com/protocolbuffers/protobuf/v3.9.0/src/google/protobuf/empty.proto -P $DIR
 fi
 
-protoc -I rpc/ -I vendor_proto/protobuf/src/ rpc/rpc.proto --go_out=plugins=grpc:rpc
+# instance manager
+python3 -m grpc_tools.protoc -I pkg/rpc -I proto/vendor/protobuf/src/ --python_out=integration/rpc/instance_manager --grpc_python_out=integration/rpc/instance_manager pkg/rpc/rpc.proto
+protoc -I pkg/rpc/ -I proto/vendor/protobuf/src/ pkg/rpc/rpc.proto --go_out=plugins=grpc:pkg/rpc
