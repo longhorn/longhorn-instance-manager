dd4a3e5ccc9ef1201f35d873c1776df184e33128
Sheng Yang <sheng.yang@rancher.com>
2020-02-25 12:01:03 -0800

    Implement VersionGet()
    Signed-off-by: Sheng Yang <sheng.yang@rancher.com>

--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/longhorn/longhorn-instance-manager/app/cmd"
+	"github.com/longhorn/longhorn-instance-manager/pkg/meta"
+)
+
+// following variables will be filled by `-ldflags "-X ..."`
+var (
+	Version   string
+	GitCommit string
+	BuildDate string
 )
 
 func main() {
 	a := cli.NewApp()
+
+	a.Version = Version
+	meta.Version = Version
+	meta.GitCommit = GitCommit
+	meta.BuildDate = BuildDate
+
 	a.Before = func(c *cli.Context) error {
 		if c.GlobalBool("debug") {
 			logrus.SetLevel(logrus.DebugLevel)
@@ -29,6 +43,7 @@ func main() {
 	a.Commands = []cli.Command{
 		cmd.StartCmd(),
 		cmd.ProcessCmd(),
+		VersionCmd(),
 	}
 	if err := a.Run(os.Args); err != nil {
 		logrus.Fatal("Error when executing command: ", err)
--- pkg/process/version.go
+++ pkg/process/version.go
@@ -1,14 +1,21 @@
 package process
 
 import (
-	"fmt"
-
 	"github.com/golang/protobuf/ptypes/empty"
 	"golang.org/x/net/context"
 
+	"github.com/longhorn/longhorn-instance-manager/pkg/meta"
 	"github.com/longhorn/longhorn-instance-manager/pkg/rpc"
 )
 
 func (pm *Manager) VersionGet(ctx context.Context, empty *empty.Empty) (*rpc.VersionResponse, error) {
-	return nil, fmt.Errorf("Unimplemented")
+	v := meta.GetVersion()
+	return &rpc.VersionResponse{
+		Version:   v.Version,
+		GitCommit: v.GitCommit,
+		BuildDate: v.BuildDate,
+
+		InstanceManagerAPIVersion:    int64(v.InstanceManagerAPIVersion),
+		InstanceManagerAPIMinVersion: int64(v.InstanceManagerAPIMinVersion),
+	}, nil
 }
