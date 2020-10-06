b51769d4d037e829286904768740fcd95ce800ea
Bo Tao <bo.tao@rancher.com>
2020-10-06 12:34:51 -0700

    Fix ticker leaking in HealthChecker 'WaitForRunning' function.
    Longhorn #1854
    
    Signed-off-by: Bo Tao <bo.tao@rancher.com>

--- pkg/process/healthchecker.go
+++ pkg/process/healthchecker.go
@@ -1,8 +1,9 @@
 package process
 
 import (
-	"github.com/sirupsen/logrus"
 	"time"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/longhorn/longhorn-instance-manager/pkg/types"
 	"github.com/longhorn/longhorn-instance-manager/pkg/util"
@@ -21,6 +22,7 @@ func (c *GRPCHealthChecker) IsRunning(address string) bool {
 
 func (c *GRPCHealthChecker) WaitForRunning(address, name string, stopCh chan struct{}) bool {
 	ticker := time.NewTicker(types.WaitInterval)
+	defer ticker.Stop()
 	for i := 0; i < types.WaitCount; i++ {
 		select {
 		case <-stopCh:
