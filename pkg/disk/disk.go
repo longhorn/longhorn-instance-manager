353b04489ea2166ffff1cad64677a64d438d71b9
Derek Su <derek.su@suse.com>
2023-06-12 13:06:42 +0000

    spdk/proxy: update methods and client apis
    Longhorn 6067
    Longhorn 6071
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/disk/disk.go
+++ pkg/disk/disk.go
@@ -144,7 +144,7 @@ func (s *Server) DiskGet(ctx context.Context, req *rpc.DiskGetRequest) (*rpc.Dis
 		"diskPath": req.DiskPath,
 	})
 
-	log.Info("Getting disk info")
+	log.Debug("Getting disk info")
 
 	if req.DiskName == "" || req.DiskPath == "" {
 		return nil, grpcstatus.Error(grpccodes.InvalidArgument, "disk name and disk path are required")
@@ -171,7 +171,7 @@ func (s *Server) DiskReplicaInstanceList(ctx context.Context, req *rpc.DiskRepli
 		"diskName": req.DiskName,
 	})
 
-	log.Info("Listing disk replica instances")
+	log.Debug("Listing disk replica instances")
 
 	if req.DiskName == "" {
 		return nil, grpcstatus.Error(grpccodes.InvalidArgument, "disk name is required")
