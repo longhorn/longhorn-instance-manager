53d5aab6fe009f4c6803575e3045098df8c5c523
Derek Su <derek.su@suse.com>
2024-03-08 06:28:32 +0000

    Update arguments of EngineSnapshotCreate
    Longhorn 8129
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit 28ec1b59c683034bc672085de7f0049413cdf325)

--- pkg/proxy/snapshot.go
+++ pkg/proxy/snapshot.go
@@ -68,7 +68,7 @@ func (ops V2DataEngineProxyOps) VolumeSnapshot(ctx context.Context, req *rpc.Eng
 		snapshotName = util.UUID()
 	}
 
-	_, err = c.EngineSnapshotCreate(req.ProxyEngineRequest.EngineName, snapshotName)
+	_, err = c.EngineSnapshotCreate(req.ProxyEngineRequest.EngineName, snapshotName, nil)
 	if err != nil {
 		return nil, grpcstatus.Errorf(grpccodes.Internal, errors.Wrapf(err, "failed to create snapshot %v", snapshotName).Error())
 	}
